55565348bbb16e6aa4bc1de2a96cb01692248cc9
utsavbgp <utsavforever05@gmail.com>
2015-06-18 01:26:06 +0530

    Making final changes

--- sgd.jl
+++ sgd.jl
@@ -12,52 +12,48 @@ function gradient(theta::Array{Float64}, x::Array{Float64}, y::Float64)
 end
 
 # update the weights using gradient descent
-function updateWeights(theta::Array{Float64}, x::Array{Float64}, y::Float64, learningRate::Float64)
+function updateWeights(theta::Array{Float64}, x::Array{Float64}, y::Float64, learningRate::Real)
     theta = theta - learningRate .* gradient(theta, x, y)
 end
 
 # return predictions for a column
-function getPred( signalCol::DataArray{Float64,1}, windowSize::Int64, learningRate::Float64, intercept::Bool, decayfactor::Float64, errorWindow::Int64)
+function getPred( signalCol::DataArray{Float64,1}, windowSize::Int64, learningRate::Real, intercept::Bool, errorWindow::Int64, logBias::Bool)
+    # initializing the Auto Regressive Part
     if intercept
         theta = ones(windowSize+1)
         x = zeros(windowSize+1)
-        #decay = ones(windowSize)
         x[windowSize+1] = 1
         theta[windowSize+1] = 0
-        #decay[windowSize+1] = 1
     else
         theta = ones(windowSize)
         x = zeros(windowSize)
-        decay = ones(windowSize)
     end
     
+    # intializing the moving average part
     phi = ones(errorWindow)
     e   = zeros(errorWindow)
     predCol = zeros(length(signalCol))
     
     theta = [theta, phi]
     for i in 1:(length(signalCol) - 1)
-        if i <= windowSize
-            x[i] = signalCol[i]
-            #decay[i] = decayfactor ^ i
-        else
+        if (windowSize > 0)
             if intercept
-                x = [x[2:windowSize], signalCol[i], x[windowSize + 1]]
+                x = [signalCol[i], x[1:(windowSize-1)], x[windowSize + 1]]
             else
-                x = [x[2:windowSize], signalCol[i]]
+                # removing the last element and right shifting the array by one element
+                x = [signalCol[i], x[1:(windowSize-1)]]
             end
         end
         
-        if i <= errorWindow
-            e[i] = signalCol[i] - predCol[i]
-        else
-            e = [e[2:errorWindow], (signalCol[i] - predCol[i])]
+        if (errorWindow > 0)
+            e = [(signalCol[i] - predCol[i]), e[1:(errorWindow-1)]]
         end
 
         predCol[i+1] = hypothesis(theta, [x, e])   
         theta = updateWeights(theta, [x, e], signalCol[i+1], learningRate)
         
-        if i > 3
+        # log bias Correction
+        if logBias & (i > 3)
             mse = sum((predCol[2:i] - signalCol[2:i]).^2)/(i-3)
             predCol[i+1] = predCol[i+1] + mse/2
         end
@@ -65,23 +61,24 @@ function getPred( signalCol::DataArray{Float64,1}, windowSize::Int64, learningRa
     return predCol
 end
 
-function getPredSignals(dt::DataFrame, windowSize::Int64, learningRate::Float64, intercept::Bool, decayFactor::Float64, errorWindow::Int64)
+# Predict log returns for each signal
+function getPredSignals(dt::DataFrame, windowSize::Int64, learningRate::Real, intercept::Bool, errorWindow::Int64, logBias::Bool)
     dt = sort(dt, cols = :date)
     colNames = names(dt)
     nsignals = size(dt)[2] - 1
     colNames = colNames[2:(nsignals+1)]
     
     predSignal = DataFrame()
+    #getting predicted log returns for each signal
     for col in colNames
-        predSignal[col] = getPred(dt[col], windowSize, learningRate, intercept, decayFactor, errorWindow::Int64)
+        predSignal[col] = getPred(dt[col], windowSize, learningRate, intercept, errorWindow, logBias)
     end
     
     return predSignal
-    #w = ones(n) ./ n #initialising with equal weight
-    
 end
 
-function evalWeights(signalVec::Array{Float64}, balancing_Factor::Float64)
+# Evaluate the weight of each signal at a given date
+function evalWeights(signalVec::Array{Float64}, balancing_Factor::Real)
     pos_loc = signalVec .>= 0
     PositiveSignal = signalVec[pos_loc]
     NegativeSignal = signalVec[!pos_loc]
@@ -101,14 +98,13 @@ function evalWeights(signalVec::Array{Float64}, balancing_Factor::Float64)
     return weight
 end
 
-function main(dt)
-    #dt = DataFrames.readtable("F:\input_combination_signals.csv")
-    predSignal = getPredSignals(dt, 252, 0.5, false, 1.0, 50)
+function main(dt::DataFrame, windowSize::Int64, learningRate::Real, intercept::Bool, errorWindow::Int64, logBias::Bool, balancingFactor::Real)
+    predSignal = getPredSignals(dt, windowSize, learningRate, intercept, errorWindow, logBias)
     shape_predSignal = size(predSignal)
     weights = zeros(shape_predSignal[1], shape_predSignal[2])
     
     for i in 1:size(predSignal)[1]
-        weights[i,:] = evalWeights(array(predSignal[i,:]), 0.8)
+        weights[i,:] = evalWeights(array(predSignal[i,:]), balancingFactor)
     end
     return weights
 end
