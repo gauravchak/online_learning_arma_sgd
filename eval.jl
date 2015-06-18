d6305f8a39965b56e9bafc84eaaa9fedcb1b17a5
Gaurav Chakravorty <gchak@circulumvite.com>
2015-06-18 19:48:49 +0000

    1. Temporarily commented out the doc macro. 2. Switch to using the array instead of the heavy dataframe object

--- eval.jl
+++ eval.jl
@@ -28,22 +28,22 @@ function getPerformanceStats(_logret_matrix, _weight_matrix)
     _sharpe = perfStats[3]
     
     println("Annualise_percent_returns are $_annualized_percent_returns Annualise_percent_stdev is $_annualized_percent_stdev Sharpe is $_sharpe")
+    return (_log_returns)
 end
 
-function evaluate(_ret_frame)
+function compute_weights_matrix(_logret_matrix)
     # Implement/call your functions here, feel free to import other modules
-    _weights_matrix = main(_ret_frame, 200, 0.5, false, 50, true, 0.8)
+    _weights_matrix = get_signal_weights(_logret_matrix, 200, 0.5, false, 50, true, 0.8)
     return _weights_matrix
 end
 
 function __init__(path)
         _returns_data_filename = path
-        _ret_frame = DataFrames.readtable(_returns_data_filename)
-#        _logret_matrix = array(_ret_frame[2:size(_ret_frame)[2]])
+        _ret_frame = DataFrames.readtable(_returns_data_filename);
         _logret_matrix = convert(Array,(_ret_frame[2:size(_ret_frame)[2]]))
         
-        for name in names(_ret_frame)[2:length(_ret_frame)]
-            perfStats = perfstats(_ret_frame[name])
+        for i = 1:size(_logret_matrix[2])
+            perfStats = perfstats(_logret_matrix[:,i])
             _annualized_percent_returns = perfStats[1]
             _annualized_percent_stdev = perfStats[2]
             _sharpe = perfStats[3]
@@ -51,9 +51,14 @@ function __init__(path)
             println("$name Annualise_percent_returns are $_annualized_percent_returns Annualise_percent_stdev is $_annualized_percent_stdev Sharpe is $_sharpe")
         end
         
-        _weight_matrix = evaluate(_ret_frame)
-        getPerformanceStats(_logret_matrix, _weight_matrix)
+        _weight_matrix = compute_weights_matrix(_logret_matrix)
+        _combined_log_returns = getPerformanceStats(_logret_matrix, _weight_matrix)
 
 end
 
+
+if ( length(ARGS) < 1 )
+    println("We need at least one argument, the path of the input file!")
+    exit(0)
+end
 __init__(ARGS[1])
--- sgd.jl
+++ sgd.jl
@@ -1,68 +1,68 @@
 using DataFrames
 
-@doc doc"""
-@Function: hypothesis
-find the value of the linear hypothesis function trans(theta) * x
+# @doc doc"""
+# @Function: hypothesis
+# find the value of the linear hypothesis function trans(theta) * x
 
-@param theta: Array of theta vector, i.e., [a_1, a_2, ....., a_p, c, b_1, b_2, ......, b_q]
-@param     x: Array consisting of previous values of signal and previous prediction errors, i.e., [x[t-1], x[t-2], ......., x[t-p], 1, e[t-1], e[t-2], ........., e[t-q]]
+# @param theta: Array of theta vector, i.e., [a_1, a_2, ....., a_p, c, b_1, b_2, ......, b_q]
+# @param x: Array consisting of previous values of signal and previous prediction errors, i.e., [x[t-1], x[t-2], ......., x[t-p], 1, e[t-1], e[t-2], ........., e[t-q]]
 
-@output returns trans(theta) * x
-""" -> 
+# @output returns trans(theta) * x
+# """ -> 
 function hypothesis(theta::Array{Float64}, x::Array{Float64})
     return (theta' * x)[1]
 end
 
-@doc doc"""
-@Function: gradient
-compute the stochastic gradient of L2-norm loss function
+# @doc doc"""
+# @Function: gradient
+# compute the stochastic gradient of L2-norm loss function
 
-@param theta: Array of theta vector, i.e., [a_1, a_2, ....., a_p, c, b_1, b_2, ......, b_q]
-@param     x: Array consisting of previous values of signal and previous prediction errors, i.e., [x[t-1], x[t-2], ......., x[t-p], 1, e[t-1], e[t-2], ........., e[t-q]]
-@param     y: Float value containing actual value of the signal
+# @param theta: Array of theta vector, i.e., [a_1, a_2, ....., a_p, c, b_1, b_2, ......, b_q]
+# @param x: Array consisting of previous values of signal and previous prediction errors, i.e., [x[t-1], x[t-2], ......., x[t-p], 1, e[t-1], e[t-2], ........., e[t-q]]
+# @param y: Float value containing actual value of the signal
 
-@function calls: hypothesis
+# @function calls: hypothesis
 
-@output returns an Array of gradients for each predictor variables x[t-1], x[t-2], ...x[t-p], intercept, e[t-1], e[t-2], ......., e[t-q]
-""" -> 
+# @output returns an Array of gradients for each predictor variables x[t-1], x[t-2], ...x[t-p], intercept, e[t-1], e[t-2], ......., e[t-q]
+# """ -> 
 function gradient(theta::Array{Float64}, x::Array{Float64}, y::Float64)
     hyp = hypothesis(theta, x)
     return (hyp - y) .* x
 end
 
-@doc doc"""
-@Function: updateWeights
-Update the theta vector using stochastic gradient descent
+# @doc doc"""
+# @Function: updateWeights
+# Update the theta vector using stochastic gradient descent
 
-@param theta: Array of theta vector, i.e., [a_1, a_2, ....., a_p, c, b_1, b_2, ......, b_q]
-@param     x: Array consisting of previous values of signal and previous prediction errors, i.e., [x[t-1], x[t-2], ......., x[t-p], 1, e[t-1], e[t-2], ........., e[t-q]]
-@param     y: Float value containing actual value of the signal
-@param learningRate
+# @param theta: Array of theta vector, i.e., [a_1, a_2, ....., a_p, c, b_1, b_2, ......, b_q]
+# @param x: Array consisting of previous values of signal and previous prediction errors, i.e., [x[t-1], x[t-2], ......., x[t-p], 1, e[t-1], e[t-2], ........., e[t-q]]
+# @param y: Float value containing actual value of the signal
+# @param learningRate
 
-@function calls: gradient
+# @function calls: gradient
 
-@output returns updated theta vector theta = theta - learningRate * gradient(theta, x, y)
-""" -> 
+# @output returns updated theta vector theta = theta - learningRate * gradient(theta, x, y)
+# """ -> 
 function updateWeights(theta::Array{Float64}, x::Array{Float64}, y::Float64, learningRate::Real)
     theta = theta - learningRate .* gradient(theta, x, y)
 end
 
-@doc doc"""
-@Function: getPred
-Computes and rerturns the predicted values of the log returns of the input signal
+# @doc doc"""
+# @Function: getPred
+# Computes and rerturns the predicted values of the log returns of the input signal
 
-@param   signalCol: Data Array containing actual values of log return of the signal on each date
-@param  windowSize: Order of the Auto-Regressive Model, i.e., the value of p in the model equation
-@param learningRate
-@param   intercept: Boolean variable to select if intercept is part of model equation, i.e., if set to false the model equation won't have c in it.
-@param errorWindow: Order of the Moving-Average Model, i.e., the value of q in the model equation
-@param 	   logBias: Boolean Operator to select if log bias correction has to be used
+# @param signalCol: Data Array containing actual values of log return of the signal on each date
+# @param windowSize: Order of the Auto-Regressive Model, i.e., the value of p in the model equation
+# @param learningRate
+# @param intercept: Boolean variable to select if intercept is part of model equation, i.e., if set to false the model equation won't have c in it.
+# @param errorWindow: Order of the Moving-Average Model, i.e., the value of q in the model equation
+# @param logBias: Boolean Operator to select if log bias correction has to be used
 
-@function calls: hypothesis: calculate the predicted value of the log returns of the signal
-@function calls: updateWeights
+# @function calls: hypothesis: calculate the predicted value of the log returns of the signal
+# @function calls: updateWeights
 
-@output predicted log returns on each date
-""" -> 
+# @output predicted log returns on each date
+# """ -> 
 function getPred( signalCol::DataArray{Float64,1}, windowSize::Int64, learningRate::Real, intercept::Bool, errorWindow::Int64, logBias::Bool)
     # initializing the Auto Regressive Part
     if intercept
@@ -107,45 +107,42 @@ function getPred( signalCol::DataArray{Float64,1}, windowSize::Int64, learningRa
     return predCol
 end
 
-@doc doc"""
-@Function getPredSignals
-Computes the predicted log returns of each signal
+# @doc doc"""
+# @Function getPredSignals
+# Computes the predicted log returns of each signal
 
-@param  		dt: Data Frame of signals
-@param  windowSize: Order of the Auto-Regressive Model, i.e., the value of p in the model equation
-@param learningRate
-@param   intercept: Boolean variable to select if intercept is part of model equation, i.e., if set to false the model equation won't have c in it.
-@param errorWindow: Order of the Moving-Average Model, i.e., the value of q in the model equation
-@param 	   logBias: Boolean Operator to select if log bias correction has to be used
+# @param _logret_matrix: Matrix of returns of each strategy
+# @param windowSize: Order of the Auto-Regressive Model, i.e., the value of p in the model equation
+# @param learningRate
+# @param intercept: Boolean variable to select if intercept is part of model equation, i.e., if set to false the model equation won't have c in it.
+# @param errorWindow: Order of the Moving-Average Model, i.e., the value of q in the model equation
+# @param logBias: Boolean Operator to select if log bias correction has to be used
 
-@function calls: getPred: compute the predicted log returns of each signal
+# @function calls: getPred: compute the predicted log returns of each signal
 
-@output Data Frame of predicted values of the signals on each date
-""" -> 
-function getPredSignals(dt::DataFrame, windowSize::Int64, learningRate::Real, intercept::Bool, errorWindow::Int64, logBias::Bool)
-    dt = sort(dt, cols = :date)
-    colNames = names(dt)
-    nsignals = size(dt)[2] - 1
-    colNames = colNames[2:(nsignals+1)]
+# @output Matrix of predicted values of the signals on each date
+# """ -> 
+function getPredSignals( _logret_matrix::Array{Float64,2}, windowSize::Int64, learningRate::Real, intercept::Bool, errorWindow::Int64, logBias::Bool)
+    nsignals = size(_logret_matrix)[2]
     
-    predSignal = DataFrame()
+    predSignal = zeros(_logret_matrix)
     #getting predicted log returns for each signal
-    for col in colNames
-        predSignal[col] = getPred(dt[col], windowSize, learningRate, intercept, errorWindow, logBias)
+    for i = 1:nsignals
+        predSignal[:,i] = getPred(_logret_matrix[i], windowSize, learningRate, intercept, errorWindow, logBias)
     end
     
     return predSignal
 end
 
-@doc doc"""
-@Function evalWeights
-Evaluate the weights of various signals of the portfolio, on every date
+# @doc doc"""
+# @Function evalWeights
+# Evaluate the weights of various signals of the portfolio, on every date
 
-@param 		  signalVec: Array of values of log returns of all signals on a date
-@param balancing_Factor
+# @param signalVec: Array of values of log returns of all signals on a date
+# @param balancing_Factor
 
-@output returns the array of weights of all signals on the date
-""" ->
+# @output returns the array of weights of all signals on the date
+# """ ->
 function evalWeights(signalVec::Array{Float64}, balancing_Factor::Real)
     pos_loc = ( signalVec .>= 0 )
     PositiveSignal = signalVec[pos_loc]
@@ -160,31 +157,31 @@ function evalWeights(signalVec::Array{Float64}, balancing_Factor::Real)
     end
     
     sumPositive = sum(PositiveSignal)
-    weight[pos_loc] =  sumPositive > 0 ? (balancing_Factor * PositiveSignal / sumPositive) : (balancing_Factor/length(PositiveSignal))
+    weight[pos_loc] = sumPositive > 0 ? (balancing_Factor * PositiveSignal / sumPositive) : (balancing_Factor/length(PositiveSignal))
     weight[!pos_loc] = 1 ./ (NegativeSignal*sum(1 ./ NegativeSignal)) * (1 - balancing_Factor)
     
     return weight
 end
 
-@doc doc"""
-@Function main
-A wrapper function which calls getPredSignals and evalWeights
+# @doc doc"""
+# @Function get_signal_weights
+# A wrapper function which calls getPredSignals and evalWeights
 
-@param  		dt: Data Frame of signals
-@param  windowSize: Order of the Auto-Regressive Model, i.e., the value of p in the model equation
-@param learningRate
-@param   intercept: Boolean variable to select if intercept is part of model equation, i.e., if set to false the model equation won't have c in it.
-@param errorWindow: Order of the Moving-Average Model, i.e., the value of q in the model equation
-@param 	   logBias: Boolean Operator to select if log bias correction has to be used
-@param balancing_Factor
+# @param _logret_matrix: 2-dimensional Array (matrix) of signals
+# @param windowSize: Order of the Auto-Regressive Model, i.e., the value of p in the model equation
+# @param learningRate
+# @param intercept: Boolean variable to select if intercept is part of model equation, i.e., if set to false the model equation won't have c in it.
+# @param errorWindow: Order of the Moving-Average Model, i.e., the value of q in the model equation
+# @param logBias: Boolean Operator to select if log bias correction has to be used
+# @param balancing_Factor
 
-@function calls: getPredSignals
-@function calls: evalWeights
+# @function calls: getPredSignals
+# @function calls: evalWeights
 
-@output returns the weight of each signal at all dates
-""" ->
-function main(dt::DataFrame, windowSize::Int64, learningRate::Real, intercept::Bool, errorWindow::Int64, logBias::Bool, balancingFactor::Real)
-    predSignal = getPredSignals(dt, windowSize, learningRate, intercept, errorWindow, logBias)
+# @output returns the weight of each signal at all dates
+# """ ->
+function get_signal_weights(_logret_matrix::Array{Float64,2}, windowSize::Int64, learningRate::Real, intercept::Bool, errorWindow::Int64, logBias::Bool, balancingFactor::Real)
+    predSignal = getPredSignals(_logret_matrix, windowSize, learningRate, intercept, errorWindow, logBias)
     shape_predSignal = size(predSignal)
     weights = zeros(shape_predSignal[1], shape_predSignal[2])
     
